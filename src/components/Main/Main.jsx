0b53fabd9cbb9384a936c40e486adb411c766554
gomess99 <gomess99@outlook.com>
2023-12-29 18:45:25 -0300

    Add main e tratando responsividade

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Main.css";
+
+function Main() {
+  return (
+    <div className="main">
+      <h2>OI, SOU <span>GABRIEL GOMES</span></h2>
+      <div className="main-text-icon">
+        <h1 className="titulo1">REACT</h1>
+        <div className="main-icon">
+          <i className="bi bi-linkedin"></i>
+          <i className="bi bi-github"></i>
+          <i className="bi bi-discord"></i>
+        </div>
+      </div>
+      <h1 className="titulo2">DEVELOPER</h1>
+      <h3>
+        Um jovem estudante empenhado em aprender e desenvolver <span>aplicações</span> e
+        <span>designes web</span>
+      </h3>
+    </div>
+  );
+}
+
+export default Main;
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./Home.css";
 import Navbar from "../../Navbar/Navbar";
 import Footer from "../../Footer/Footer";
+import Main from "../../Main/Main";
 
 function Home(){
     return(
         <div>
             <Navbar />
+            <Main />
             <Footer />
         </div>
     )
